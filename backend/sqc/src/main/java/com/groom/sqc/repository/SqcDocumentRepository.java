ed6e53c55a9578507f94267412720d5ff894be7b
jeonghun <wjdgns2751@naver.com>
2024-01-23 17:23:43 +0900

    아이디의 범위값을 제공 받아서 해당범위의 리스트를 조회하는 메서드 생성

--- backend/sqc/src/main/java/com/groom/sqc/repository/SqcDocumentRepository.java
+++ backend/sqc/src/main/java/com/groom/sqc/repository/SqcDocumentRepository.java
@@ -1,11 +1,17 @@
 package com.groom.sqc.repository;
 
 import com.groom.sqc.domain.documents.SqcDocument;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
-public interface SqcDocumentRepository extends MongoRepository<SqcDocument, Long> {
-
+public interface SqcDocumentRepository extends MongoRepository<SqcDocument, String> {
+    // 범위에 해당하는 ID를 조회하는 커스텀 쿼리 메서드
+    @Query("{ '_id': { $gte: ?0, $lte: ?1 } }")
+    List<SqcDocument> findByIdRange(ObjectId startId, ObjectId endId);
 }
