704d84adc991e112acf72f634af871012a032697
jeonghun <wjdgns2751@naver.com>
2024-01-23 17:29:34 +0900

    1. 신발 리스트 조회 기능  (1). 파이썬에 파라미터 제공  (2). 파이썬 서버에서 반환받는 값으로 MongoDB atlas 에서 조회 / ex) { 'startId' : 'sasdfkljasdfasdf34' , 'endId' : 'afsdasdfasdfasdf33'} 받는 경우 2. 신발 조회 기능

--- backend/sqc/src/main/java/com/groom/sqc/services/SqcService.java
+++ backend/sqc/src/main/java/com/groom/sqc/services/SqcService.java
@@ -7,72 +7,73 @@ import com.groom.sqc.domain.dto.SqcResponseDto;
 import com.groom.sqc.repository.SqcDocumentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.webjars.NotFoundException;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class SqcService {
     private final SqcDocumentRepository sqcDocumentRepository;
-    ApiServerHttpClient apiServerHttpClient;
+    private final ApiServerHttpClient apiServerHttpClient;
 
     public List<SqcResponseDto> shoesList(SqcRequestDto sqcRequestDto) {
         // 1. 요청 들어온 데이터를 Python에게 전달 후 리턴값으로 몽고 DB에 저장된 키 값을 응답 받음
-        ResponseEntity<String> responseEntity  = apiServerHttpClient.sendPostRequest("/findShoes",sqcRequestDto);
-        log.info("response status : {}",responseEntity.getStatusCode());
-        log.info("response value : {}",responseEntity.getBody());
-        // 입력 문자열을 ":"를 기준으로 분리하여 id를 추출
+        ResponseEntity<String> responseEntity = apiServerHttpClient.sendPostRequest("/findShoes", sqcRequestDto);
+        log.info("response status : {}", responseEntity.getStatusCode());
+        log.info("response value : {}", responseEntity.getBody());
+
         String body = responseEntity.getBody();
 
-
+        // 입력 문자열을 ":"를 기준으로 분리하여 id를 추출
         assert body != null;
         String[] tokens = body.split(",");
-        Iterable<Long> idRange = null;
+        String startToken = extractValue(tokens[0]);
+        String endToken = extractValue(tokens[1]);
 
-        if (tokens.length == 2) {
-            String startToken = tokens[0].split(":")[1].trim();
-            String endToken = tokens[1].split(":")[1].trim();
-
-            try {
-                int startId = Integer.parseInt(startToken);
-                int endId = Integer.parseInt(endToken);
-
-                idRange = IntStream.rangeClosed(startId, endId)
-                        .boxed()
-                        .map(Long::valueOf)
-                        .collect(Collectors.toList());
-
-            } catch (NumberFormatException e) {
-                e.fillInStackTrace();
-            }
-        }
-
+        ObjectId objectStart = new ObjectId(startToken);
+        ObjectId objectEnd = new ObjectId(endToken);
         // 2. 연결된 몽고 DB에서 키 값을 기준으로 조회후 List<SqcResponseDto>로 반환
-        // SqcVo를 생성하고, 이를 이용하여 SqcResponseDto를 초기화합니다.
-
         // 몽고디비에서 데이터를 조회하고 SqcVo 리스트를 얻어옵니다.
-        assert idRange != null;
         // ID 범위 안의 sqcDocument 를 sqcResponseDto 형식으로 변환 후 리스트화
-        return sqcDocumentRepository.findAllById(idRange)
+        Optional<SqcDocument> a = sqcDocumentRepository.findById(startToken);
+        Optional<SqcDocument> b = sqcDocumentRepository.findById(endToken);
+        log.info("SqcDocument a  {} : ",a.orElse(new SqcDocument()));
+        log.info("SqcDocument b  {} : ",b.orElse(new SqcDocument()));
+
+        List<SqcResponseDto> sqcResponseDtoList = sqcDocumentRepository.findByIdRange(objectStart, objectEnd)
                 .stream()
                 .map(SqcResponseDto::new)
                 .collect(Collectors.toList());
 
+        log.info("scqResponseDtoList {} : ",sqcResponseDtoList);
+
+
+    return sqcResponseDtoList;
 
     }
 
-    public SqcResponseDto shoes(Long id){
+    public SqcResponseDto shoes(String id) {
         Optional<SqcDocument> sqcDocument = sqcDocumentRepository.findById(id);
         return sqcDocument
                 .map(SqcResponseDto::new)
                 .orElseThrow(() -> new NotFoundException("SqcDocument not found with id: " + id));
-     }
+    }
+
+
+    private static String extractValue(String token) {
+        return Arrays.stream(token.split(":"))
+                .skip(1)
+                .findFirst()
+                .map(String::trim)
+                .orElse("");
+    }
 
 }
