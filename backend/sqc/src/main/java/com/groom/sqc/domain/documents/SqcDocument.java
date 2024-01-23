10df8af4b2efe9ed420fd0f0c8fdf676c74fbea9
jeonghun <wjdgns2751@naver.com>
2024-01-23 17:24:39 +0900

    MongoDB atlas 에 저장되어 있는 shoes 컬렉션에 매핑되는 Document 생성

--- backend/sqc/src/main/java/com/groom/sqc/domain/documents/SqcDocument.java
+++ backend/sqc/src/main/java/com/groom/sqc/domain/documents/SqcDocument.java
@@ -3,28 +3,31 @@ package com.groom.sqc.domain.documents;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+import java.util.Map;
 
-@Document(collection = "sqcCollection")
+@Document(collection = "shoes")
 @AllArgsConstructor
 @Getter
 @RequiredArgsConstructor
 public class SqcDocument {
 
     @Id
-    private Long id;
-
+    private ObjectId id;
     private String brand;
-    private String name;
-    private List<String> tags;
-    private String sex;
-    private String price;
-    private String view;
     private String buy;
+    private String like;
+    private String name;
+    private String price;
     private String review;
+    private String sex;
+    private List<Map<String, List<String>>> size;
+    private List<String> tag;
     private String url;
+    private String view;
 
 }
