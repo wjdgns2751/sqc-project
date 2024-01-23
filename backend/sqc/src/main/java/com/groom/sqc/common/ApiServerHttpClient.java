a2e9bd1a81f6d4ee2dff04daf149799f4e402a92
jeonghun <wjdgns2751@naver.com>
2024-01-23 17:26:37 +0900

    Api 요청을 위한 인터페이스를 파이썬서버 호출을 위하여 구현

--- backend/sqc/src/main/java/com/groom/sqc/common/ApiServerHttpClient.java
+++ backend/sqc/src/main/java/com/groom/sqc/common/ApiServerHttpClient.java
@@ -9,21 +9,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import static com.groom.sqc.common.ServerProperties.PYTHON_HOST_NAME;
-
 @Component("apiServerHttpClient")
 @RequiredArgsConstructor
 @Slf4j
 public class ApiServerHttpClient implements ServerHttpClient {
-    StringBuilder stringBuilder = new StringBuilder();
 
+    private final ServerProperties serverProperties ;
+    StringBuilder stringBuilder = new StringBuilder();
     HttpHeaders headers = new HttpHeaders();
 
     @Override
     public ResponseEntity<String> sendPostRequest(String url, Object requestData) {
+        stringBuilder.setLength(0);
+        headers.add("Content-Type", "application/json");
         // Python 서버에 특화된 동작을 구현
-        stringBuilder.append(PYTHON_HOST_NAME);
+        stringBuilder.append(serverProperties.getPythonHostName());
         stringBuilder.append(url);
+        log.info("header {}", headers);
         log.info("url {}", stringBuilder);
         // RestTemplate 을 사용하여 Python 서버로 POST 요청 보내기
         RestTemplate restTemplate = new RestTemplate();
