5a87938d178c5002be10ab9fc6526b7de9d77db5
DONG GON YU <136345422+gonieyoo@users.noreply.github.com>
2024-01-25 19:18:58 +0900

    Add files via upload
    Signed-off-by: DONG GON YU <136345422+gonieyoo@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,47 @@
 import "./App.css";
 import MainPageComponent from "./main";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+
 function App() {
-  return <MainPageComponent />;
+  const history = useHistory();
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <Link to="/">
+            <img src="/images/icons/logo2.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path="/">
+            <MainPageComponent />
+          </Route>
+          <Route exact={true} path="/products/:id">
+            <ProductPage />
+          </Route>
+          <Route exact={true} path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/main/index.js
+++ frontend/src/main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
@@ -20,40 +21,34 @@ function MainPage() {
 
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo2.png" />
-        </div>
+      <div id="banner">
+        <img src="images/banners/banner3.png" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner3.png" />
-        </div>
-        <h1>판매되는상품들</h1>
-        <div id="product-list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product-card">
+      <h1>판매되는상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link className="product-link" to={`/products/${product.id}`}>
                 <div>
                   <img className="product-img" src={product.imageUrl} />
                 </div>
                 <div className="product-contents">
                   <span className="product-name">{product.name}</span>
                   <span className="product-price">{product.price}원</span>
-                  <span className="product-seller">
+                  <div className="product-seller">
                     <img
                       className="product-avatar"
                       src="images/icons/avatar.png"
                     />
                     <span>{product.seller}</span>
-                  </span>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div id="footer"></div>
     </div>
   );
 }
