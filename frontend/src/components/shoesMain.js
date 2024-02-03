3811dcd420f1fe42fb8d1e9fb900a53036b67391
jeonghun <wjdgns2751@naver.com>
2024-02-03 12:22:12 +0900

    로컬 테스트 환경 추가

--- frontend/src/components/shoesMain.js
+++ frontend/src/components/shoesMain.js
@@ -10,13 +10,22 @@ dayjs.extend(relativeTime);
 dayjs.locale("ko");
 
 function MainComponent() {
-  const [products, setProducts] = React.useState([]);
+  // const [products, setProducts] = React.useState([]);
+  const [sqcDocuments, setSqcDocuments] = React.useState([]);
   React.useEffect(function () {
     axios
       .get(`${API_URL}/findShoesList`)
       .then(function (result) {
-        const products = result.data.products;
-        setProducts(products);
+        // const products = result.data.products;
+        // setProducts(products);
+
+        const sqcDocumentList = result.data.map(item => item.sqcDocument);
+        setSqcDocuments(sqcDocumentList);
+
+
+        console.log(sqcDocumentList[0].id);
+
+
       })
       .catch(function (error) {
         console.error("에러 발생 : ", error);
@@ -24,34 +33,25 @@ function MainComponent() {
   }, []);
 
   return (
+
     <div>
       <div id="banner">
         <img src="images/banners/banner3.png" />
       </div>
       <h1>판매되는상품들</h1>
       <div id="product-list">
-        {products.map(function (product, index) {
+        {sqcDocuments.map(function (sqcDocument, index) {
+
           return (
-            <div className="product-card">
-              <Link className="product-link" to={`/products/${product.id}`}>
+            <div className="product-card" key={index}>
+              <Link className="product-link" to={`/products/${sqcDocument.id}`}>
                 <div>
-                  <img className="product-img" src={product.imageUrl} />
+                  <img className="product-img" src={sqcDocument.img} />
                 </div>
                 <div className="product-contents">
-                  <span className="product-name">{product.name}</span>
-                  <span className="product-price">{product.price}원</span>
-                  <div className="product-footer">
-                    <div className="product-seller">
-                      <img
-                        className="product-avatar"
-                        src="images/icons/avatar.png"
-                      />
-                      <span>{product.seller}</span>
-                    </div>
-                    <span className="product-date">
-                      {dayjs(product.createdAt).fromNow()}
-                    </span>
-                  </div>
+                  <span className="product-name">{sqcDocument.name}</span>
+                  <span className="product-price">{sqcDocument.price}원</span>
+                  {/* 나머지 속성들도 필요한 대로 렌더링 */}
                 </div>
               </Link>
             </div>
@@ -59,6 +59,43 @@ function MainComponent() {
         })}
       </div>
     </div>
+
+
+    // <div>
+    //   <div id="banner">
+    //     <img src="images/banners/banner3.png" />
+    //   </div>
+    //   <h1>판매되는상품들</h1>
+    //   <div id="product-list">
+    //     {products.map(function (product, index) {
+    //       return (
+    //         <div className="product-card">
+    //           <Link className="product-link" to={`/products/${product.id}`}>
+    //             <div>
+    //               <img className="product-img" src={product.imageUrl} />
+    //             </div>
+    //             <div className="product-contents">
+    //               <span className="product-name">{product.name}</span>
+    //               <span className="product-price">{product.price}원</span>
+    //               <div className="product-footer">
+    //                 <div className="product-seller">
+    //                   <img
+    //                     className="product-avatar"
+    //                     src="images/icons/avatar.png"
+    //                   />
+    //                   <span>{product.seller}</span>
+    //                 </div>
+    //                 <span className="product-date">
+    //                   {dayjs(product.createdAt).fromNow()}
+    //                 </span>
+    //               </div>
+    //             </div>
+    //           </Link>
+    //         </div>
+    //       );
+    //     })}
+    //   </div>
+    // </div>
   );
 }
 
